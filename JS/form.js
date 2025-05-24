80e0ee22cc4bb6628d15e34df19aacfc9abfb36f
Ftrx17 <kumuhdfitri@gmail.com>
2025-05-24 20:19:10 +0800

    last

--- JS/form.js
+++ JS/form.js
@@ -20,18 +20,33 @@ document.getElementById('vehicleForm').addEventListener('submit', function (e) {
     };
 
     const errors = [];
+    if (!data.name) errors.push("Nama cannot be empty.");
+    if(!data.programme_code) errors.push("Kod Program cannot be empty.");
+    if (!data.program) errors.push("Program cannot be empty.");
+    if (!data.faculty) errors.push("Fakulti cannot be empty.");
+    if (!data.semester) errors.push("Semester must be selected.");
+    if (!data.college_name) errors.push("Nama Kolej cannot be empty.");
+    if (!data.address) errors.push("Alamat cannot be empty.");
+    if (!data.license_plate) errors.push("No. Pendaftaran cannot be empty.");
+    if (!data.vehicle_type) errors.push("Jenis Kenderaan must be selected.");
+    if (!data.vehicle_color) errors.push("Warna Kenderaan cannot be empty.");
+    if (!data.excuse) errors.push("Sebab cannot be empty.");
+    if (!/^[a-zA-Z\s]+$/.test(data.name)) errors.push("Nama must contain only letters and spaces.");
+
     if (!/^\d{12}$/.test(data.id_number)) errors.push("No. K/P must be exactly 12 digits.");
     if (!/^\d+$/.test(data.phone_number)) {
-        errors.push("Phone Number must contain only digits.");
+        errors.push("No. Telefon must contain only digits.");
     } else if (data.phone_number.length !== 10) {
         errors.push("Phone Number must be exactly 10 digits.");
     }
     if (!/^\d+$/.test(data.student_id)) {
-        errors.push("Student ID must contain only digits.");
+        errors.push("No. Pelajar must contain only digits.");
     } else if (data.student_id.length !== 10) {
-        errors.push("Student ID must be exactly 10 digits.");
+        errors.push("No. Pelajar must be exactly 10 digits.");
     }
-
+    if (!/^[A-Za-z\s]+$/.test(data.program)) {
+    errors.push("Program must contain letters only (no numbers or special characters).");
+    }
     if (errors.length > 0) {
         alert("Validation Error:\n" + errors.join("\n"));
         return;
